f647fa7e6ef05c597e1f799598385bb122386826
Rakhyvel <josephmshimel@gmail.com>
2026-06-14 12:33:35 -0600

    fix the trait thing

--- src/hierarchy/compiler.zig
+++ src/hierarchy/compiler.zig
@@ -94,8 +94,10 @@ pub fn init(stderr: ?std.fs.File, alloc: std.mem.Allocator) Error!*Self {
     retval.prelude = try prelude_.get_prelude_scope(retval);
     retval.core = core_.get_core_scope(retval) catch |e| {
         if (stderr == null) return e;
-        var writer = stderr.?.writer(&.{}).interface;
-        retval.errors.print_errors(&writer, .{});
+        // Keep the file writer alive with a named local
+        // copying the interface would leave it pointing to a freed stack
+        var writer_struct = stderr.?.writer(&.{});
+        retval.errors.print_errors(&writer_struct.interface, .{});
         return e;
     };
 
