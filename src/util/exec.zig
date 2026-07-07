4d71a8145a12f6b656afb48563df3f70793a4817
Rakhyvel <josephmshimel@gmail.com>
2026-07-06 21:54:09 -0600

    fix windows

--- src/util/exec.zig
+++ src/util/exec.zig
@@ -35,11 +35,7 @@ pub fn exec(argv: []const []const u8, behavior: std.process.Child.StdIo) !struct
             retcode = c;
         },
         .Signal => |c| switch (c) {
-            std.posix.SIG.ILL => return error.IllegalInstruction,
-            std.posix.SIG.ABRT => return error.Aborted,
-            std.posix.SIG.FPE => return error.ArithmeticError,
-            std.posix.SIG.BUS => return error.BusError,
-            std.posix.SIG.SEGV => return error.SegmentationFault,
+            11 => return error.SegmentationFault,
             else => return error.UnknownSignal,
         },
         else => return error.CommandFailed,
